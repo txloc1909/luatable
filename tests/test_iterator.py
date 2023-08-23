f68e2625856088d065808f25e6526b0d53aacb74
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-08-23 15:22:26 +0700

    separate tests for iterators

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -1,7 +1,10 @@
+import pytest
+
 from luatable import Table, pairs, ipairs
 
 
-def test_iterator():
+@pytest.fixture
+def test_table():
     t = Table()
     t[-1] = "y"
     t[0] = "z"
@@ -11,19 +14,27 @@ def test_iterator():
     t[4] = "d"
     t[6] = "e"
     t["hello"] = "world"
+    return t
 
-    ipairs_output = tuple(ipairs(t))
-    pairs_output = tuple(pairs(t))
 
-    # numeric order of keys is guaranteed in `ipairs`
-    assert ipairs_output == ((1, "a"), (2, "c"), (3, "b"), (4, "d"))
+def test_pairs(test_table):
+    output = tuple(pairs(test_table))
 
     # `pairs` guarantee no order, so just check if output exists
-    assert (1, "a") in pairs_output
-    assert (2, "c") in pairs_output
-    assert (3, "b") in pairs_output
-    assert (4, "d") in pairs_output
-    assert (0, "z") in pairs_output
-    assert (6, "e") in pairs_output
-    assert ("hello", "world") in pairs_output
-    assert (-1, "y") in pairs_output
+    assert (1, "a") in output
+    assert (2, "c") in output
+    assert (3, "b") in output
+    assert (4, "d") in output
+    assert (0, "z") in output
+    assert (6, "e") in output
+    assert ("hello", "world") in output
+    assert (-1, "y") in output
+
+
+def test_ipairs(test_table):
+    output = tuple(ipairs(test_table))
+
+    # numeric order of keys is guaranteed in `ipairs`
+    expected = ((1, "a"), (2, "c"), (3, "b"), (4, "d"))
+
+    assert output == expected
