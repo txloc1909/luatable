86eed02759b09c2a58389f71c89f989403044ee6
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2024-04-14 23:59:56 +0700

    set up function signature for `table` module

--- src/luatable/table.py
+++ src/luatable/table.py
@@ -1,29 +1,37 @@
-# Implement functions from: https://www.lua.org/manual/5.4/manual.html#6.6
+"""
+Implement lua stdlib functions from:
+    https://www.lua.org/manual/5.4/manual.html#6.6
+"""
+
+import operator
+from typing import Any, Callable, Optional, Tuple
+
+from luatable import Table
 
 
-def concat():
+def concat(list_: Table, sep: str = "", i: int = 1, j: Optional[int] = None) -> str:
     return NotImplemented
 
 
-def insert():
+def insert(list_: Table, value: Any, pos: Optional[int] = None) -> None:
     return NotImplemented
 
 
-def move():
+def move(a1: Table, from_: int, end: int, to: int, a2: Optional[Table] = None) -> Table:
     return NotImplemented
 
 
-def pack():
+def pack(*args) -> Table:
     return NotImplemented
 
 
-def remove():
+def remove(list_: Table, pos: Optional[int] = None) -> Table:
     return NotImplemented
 
 
-def sort():
+def sort(list_: Table, comp: Callable[[Any, Any], bool] = operator.lt):
     return NotImplemented
 
 
-def unpack():
+def unpack(list_: Table, i: int = 1, j: Optional[int] = None) -> Tuple[Any]:
     return NotImplemented
