17644af441e9a0f82fa9ee7d825a30e8b9d0686b
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2024-04-14 22:57:41 +0700

    layout `table` module

---
+++ src/luatable/table.py
@@ -0,0 +1,29 @@
+# Implement functions from: https://www.lua.org/manual/5.4/manual.html#6.6
+
+
+def concat():
+    return NotImplemented
+
+
+def insert():
+    return NotImplemented
+
+
+def move():
+    return NotImplemented
+
+
+def pack():
+    return NotImplemented
+
+
+def remove():
+    return NotImplemented
+
+
+def sort():
+    return NotImplemented
+
+
+def unpack():
+    return NotImplemented
